527d9df8d98bd382018351071a2b7eace2b380d5
Mike D'Ambrosio <mdambrosio@Mikes-MacBook-Pro.local>
2012-08-22 15:29:28 -0700

    Initial Commit

---
+++ LoaLoaCoreData3Tests/LoaLoaCoreData3Tests.h
@@ -0,0 +1,13 @@
+//
+//  LoaLoaCoreData3Tests.h
+//  LoaLoaCoreData3Tests
+//
+//  Created by Mike D'Ambrosio on 8/22/12.
+//  Copyright (c) 2012 Mike D'Ambrosio. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LoaLoaCoreData3Tests : SenTestCase
+
+@end
---
+++ LoaLoaCoreData3Tests/LoaLoaCoreData3Tests.m
@@ -0,0 +1,32 @@
+//
+//  LoaLoaCoreData3Tests.m
+//  LoaLoaCoreData3Tests
+//
+//  Created by Mike D'Ambrosio on 8/22/12.
+//  Copyright (c) 2012 Mike D'Ambrosio. All rights reserved.
+//
+
+#import "LoaLoaCoreData3Tests.h"
+
+@implementation LoaLoaCoreData3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LoaLoaCoreData3Tests");
+}
+
+@end
